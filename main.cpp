606c4887d8e08e181d63a44e34ce017ac3f2285a
zschroeder6212@gmail.com <zschroeder6212@gmail.com>
2020-07-30 14:40:41 -0500

    added vibration

--- main.cpp
+++ main.cpp
@@ -26,29 +26,17 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#include <cstdio>
-#include <iostream>
-#include <memory>
-#include <stdexcept>
-#include <string>
-#include <array>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <poll.h>
+#include <sys/ioctl.h>
+#include <linux/input.h>
 
 #include <dirent.h>
 
 #define THRESHOLD 32000
-
-std::string exec(const char* cmd) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        result += buffer.data();
-    }
-    return result;
-}
 
 //reads file and converts to int
 int ftoi(char* path)
@@ -94,6 +82,90 @@ bool find(char* path, char* contains, char* newPath)
 
 }
 
+int open_event_dev(const char* name_needle, int flags)
+{
+	char path[256];
+	char name[256];
+	int fd, ret;
+
+	// find the right device and open it
+	for (int i = 0; i < 10; i++) {
+        snprintf(path, sizeof path, "/dev/input/event%d", i);
+        fd = open(path, flags);
+        if (fd < 0)
+            continue;
+
+        ret = ioctl(fd, EVIOCGNAME(256), name);
+        if (ret < 0)
+            continue;
+        
+        if (strstr(name, name_needle))
+            return fd;
+        
+        close(fd);
+	}
+	
+	errno = ENOENT;
+	return -1;
+}
+
+void syscall_error(int is_err, const char* fmt, ...)
+{
+	va_list ap;
+
+	if (!is_err)
+		return;
+
+	printf("ERROR: ");
+	va_start(ap, fmt);
+	vprintf(fmt, ap);
+	va_end(ap);
+	printf(": %s\n", strerror(errno));
+}
+
+
+extern "C" {
+	struct ff_effect e;
+	struct input_event play;
+}
+
+
+//vibrate code adapted from https://git.sr.ht/~mil/sxmo-utils/tree/master/programs/sxmo_vibratepine.c
+void vibrate(int durationMs, int strength)
+{
+
+    int fd, ret;
+	struct pollfd pfds[1];
+	int effects;
+
+    fd = open_event_dev("vibrator", O_RDWR | O_CLOEXEC);
+	syscall_error(fd < 0, "Can't open vibrator event device");
+	ret = ioctl(fd, EVIOCGEFFECTS, &effects);
+	syscall_error(ret < 0, "EVIOCGEFFECTS failed");
+
+	e.type = FF_RUMBLE;
+	e.id = -1;
+	e.u.rumble.strong_magnitude = strength;
+
+	ret = ioctl(fd, EVIOCSFF, &e);
+	syscall_error(ret < 0, "EVIOCSFF failed");
+
+	play.type = EV_FF;
+	play.code = e.id;
+	play.value = 3;
+
+	ret = write(fd, &play, sizeof play);
+	syscall_error(ret < 0, "write failed");
+
+	usleep(durationMs * 1000);
+
+	ret = ioctl(fd, EVIOCRMFF, e.id);
+	syscall_error(ret < 0, "EVIOCRMFF failed");
+
+	close(fd);
+}
+
+
 int main()
 {
 
@@ -102,7 +174,7 @@ int main()
     //scan for accelerometer
     if(find("/sys/bus/i2c/drivers/inv-mpu6050-i2c", "0068", accel_path))
     {
-        if(find(accel_path, "iio\:device", accel_path))
+        if(find(accel_path, "iio:device", accel_path))
         {
             sprintf(accel_path, "%s/in_accel_y_raw", accel_path);
         }else{
@@ -156,7 +228,7 @@ int main()
 
             sprintf(brightness, "%d", flash_on);
 
-            int fd = open("/sys/devices/platform/led-controller/leds/white\:flash/brightness", O_RDWR);
+            int fd = open("/sys/devices/platform/led-controller/leds/white:flash/brightness", O_RDWR);
 
             if(fd != NULL)
             {
@@ -164,6 +236,8 @@ int main()
             }
 
             close(fd);
+
+            vibrate(200, 4000);
 
             //wait 1 second before continuing to avoid multiple detections
             usleep(1000000);
