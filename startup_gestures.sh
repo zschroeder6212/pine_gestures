a53b0e8365fb634e525faeb037eca4c11823a70f
zschroeder6212@gmail.com <zschroeder6212@gmail.com>
2021-05-02 22:49:04 -0500

    removed hardcoded commands and instead added them to startup script

--- startup_gestures.sh
+++ startup_gestures.sh
@@ -1,4 +1,4 @@
 #!bin/sh
 
 # script to run on startup to have pine_gestures on every boot
-/usr/bin/pine_gestures
+/usr/bin/pine_gestures --shake_cmd /usr/bin/toggleflash --twist_cmd /usr/bin/megapixels
